e8e812441ed6281c105fdca10eb1980d315d5b54
Bradley Guidry <bradleyguidry@gmail.com>
2020-08-12 00:16:48 -0500

    adds to js file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 // WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
+/
 // WHEN the game is over
 // THEN I can save my initials and score
 // when the timer ends, the game ends and the results are displayed
